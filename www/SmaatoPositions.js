01b7aff323a6bdbdae6570836c373f0db3cd4ece
ahmedzaher96 <48657494+ahmedzaher96@users.noreply.github.com>
2020-08-02 10:59:38 +0200

    Add files via upload

---
+++ www/SmaatoPositions.js
@@ -0,0 +1,33 @@
+﻿/*
+
+Copyright 2015 Pedro Santos
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+var SMAATO_AD_POSITION = {
+    NO_CHANGE: 11,
+    TOP_LEFT: 1,
+    TOP_CENTER: 2,
+    TOP_RIGHT: 3,
+    LEFT: 4,
+    CENTER: 5,
+    RIGHT: 6,
+    BOTTOM_LEFT: 7,
+    BOTTOM_CENTER: 8,
+    BOTTOM_RIGHT: 9,
+    POS_XY: 10
+};
+
+module.exports = SMAATO_AD_POSITION;
---
+++ www/SmaatoSizes.js
@@ -0,0 +1,31 @@
+﻿/*
+
+Copyright 2015 Pedro Santos
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+var SMAATO_AD_SIZE = {
+    TINY_BANNER: 'TINY_BANNER', // Phones 120 x 20
+    PUNY_BANNER: 'PUNY_BANNER', // Phones 168 x 28
+    LITTLE_BANNER: 'LITTLE_BANNER', // Phones 216 x 36
+    SMALL_BANNER: 'SMALL_BANNER', // Phones and Tablets 300 x 50
+    BANNER: 'BANNER', // Phones and Tablets 320 x 50
+    LEADERBOARD: 'LEADERBOARD', // Tablets 728 x 90
+    SKYSCRAPER: 'SKYSCRAPER', // Tablets 120 x 600
+    MEDIUM_RECTANGLE: 'MEDIUM_RECTANGLE', // Phones and Tablets 300 x 250
+    INTERSTITIAL: 'INTERSTITIAL', // Phones and Tablets 100%
+    CUSTOM: 'CUSTOM' // Phones and Tablets custom width and height
+};
+
+module.exports = SMAATO_AD_SIZE;
